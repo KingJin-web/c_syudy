96b741de5010a351217538f64f630e5cd1688629
12613 <1261337209@qq.com>
2020-06-07 18:56:14 +0800

    测试2

---
+++ Sort/insertionSort/main.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+
+void insertion_sort(int arr[],int len){
+        for(int i=1;i<len;i++){
+                int key=arr[i];
+                int j=i-1;
+                while((j>=0) && (key<arr[j])){
+                        arr[j+1]=arr[j];
+                        j--;
+                }
+                arr[j+1]=key;
+        }
+}
+int main() {
+        int arr[] = { 22, 34, 3, 32, 82, 55, 89, 50, 37, 5, 64, 35, 9, 70 };
+        int len = (int) sizeof(arr) / sizeof(*arr);
+        insertion_sort(arr, len);
+        int i;
+        for (i = 0; i < len; i++)
+                printf("%d ", arr[i]);
+        return 0;
+}
