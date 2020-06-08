833c60770b614f1c8f27f768917b17dab26e56a1
12613 <1261337209@qq.com>
2020-06-08 18:06:23 +0800

    定义顺序表

--- A1/1.cpp
+++ A1/1.cpp
@@ -63,3 +63,5 @@ int main()
         cout << "   " << arr[i];
     return 0;
 }
+
+
---
+++ 老九之数据结构/顺序表/DataElement.h
@@ -0,0 +1,34 @@
+
+
+
+/************************************************************************
+ *  Project: 数据结构第二章案例
+ *  Function: 用来定义数据元素
+ *  Description:
+
+ ************************************************************************
+
+ ************************************************************************/
+
+ #define MAX_SIZE 255
+ #define TRUE 1
+ #define FALSE 0
+
+ //1、定义数据元素
+ //typedef int ElementType;
+ /*
+  * datas = {{1, ""}, {2, ""}, {3, ""}};
+  */
+ typedef struct{
+    int id;
+    char * name;
+ }ElementType;
+
+ //2、定义顺序表结构
+ typedef struct{
+    ElementType datas[MAX_SIZE];    //顺序表中的数据元素集合
+    int length;                     //当前顺序表中的元素个数
+ }SeqList;
+
+
+
