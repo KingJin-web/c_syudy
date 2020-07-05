bc1d8334946c39b05c5bc95bc97e1243f9391a1d
12613 <1261337209@qq.com>
2020-07-05 18:15:38 +0800

    ada

--- python/1.py
+++ python/1.py
@@ -1,72 +1,21 @@
+x = "-123456"
 
-# a = [5,7,6,3,4,1,2]
-# b = sorted(a)
-# print(b)
-# a.sort()
-# print(a.sort())
-
-# echo "# c_syudy" >> README.md
-# git init
-# git add README.md
-# git commit -m "first commit"
-# git remote add origin https://github.com/KingJin-web/c_syudy.git
-# git push -u origin master
-                
-# git remote add origin https://github.com/KingJin-web/c_syudy.git
-# git push -u origin master               
-
-# git add .
-# git commit -m "C语言数据结构"
-
-
-# 1、找到仓库，并复制地址
-
-# 2、新建一个文件
-
-# 3、点击git的git bash here
-
-# 4、执行语句：$git clone https://github.com/llsok/YC85/tree/master/C85-S2-Ply-web
+if x[0] == "-":
+    if x[1:len(x):].isdigit() :
+        print("0k")
+    else:
+        print("no")
+if x[0] != "-":
+    if x[1:len(x):].isdigit() :
+        print("ok")
+    else:
+        print("no")
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-a = 2
-b = 1
-
-c = a + b;
-
-
-print("a + b = ",c)
-print("a + b = " + c)
-
+# print(x[1:len(x):].isdigit())
+# print(x[0])
+# print(x.isdigit())
---
+++ study/Unit3/a.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+using namespace std;
+
+void a(const char *aa){
+    printf("%s",aa);
+}
+
+int main(){
+
+    a("1234156aaa");
+
+    return 0;
+}
