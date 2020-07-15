15b36e594fe5488030c5107eca7ba585b96d6b7f
12613 <1261337209@qq.com>
2020-07-15 17:21:25 +0800

    1

--- python/1.py
+++ python/1.py
@@ -1,15 +1,15 @@
-x = "-123456"
+# x = "-123456"
 
-if x[0] == "-":
-    if x[1:len(x):].isdigit() :
-        print("0k")
-    else:
-        print("no")
-if x[0] != "-":
-    if x[1:len(x):].isdigit() :
-        print("ok")
-    else:
-        print("no")
+# if x[0] == "-":
+#     if x[1:len(x):].isdigit() :
+#         print("0k")
+#     else:
+#         print("no")
+# if x[0] != "-":
+#     if x[1:len(x):].isdigit() :
+#         print("ok")
+#     else:
+#         print("no")
 
 
 
@@ -19,3 +19,6 @@ if x[0] != "-":
 # print(x[1:len(x):].isdigit())
 # print(x[0])
 # print(x.isdigit())
+
+
+print(100/.3)
