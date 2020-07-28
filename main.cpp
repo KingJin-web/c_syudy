66c0b36b51270be8658c318403d0eeb3f544d9e8
12613 <1261337209@qq.com>
2020-07-28 18:40:22 +0800

    2

--- main.cpp
+++ main.cpp
@@ -2,12 +2,16 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello，kadsj0激发");
+    
+    double a = 5000,b = 1.055,c;
+    c= a*b;
+    printf("%f",c);
     return 0;
+    
 }
-//  E:/mingw64/bin/../lib/gcc/x86_64-w64-mingw32/8.1.0/include/c++
+//  E:/mingw64/bin/../lib/gcc/x86_64-w64-mingw32/8.1.0/include/c++++++++++++
 //  E:/mingw64/bin/../lib/gcc/x86_64-w64-mingw32/8.1.0/include/c++/x86_64-w64-mingw32
 //  E:/mingw64/bin/../lib/gcc/x86_64-w64-mingw32/8.1.0/include/c++/backward
 //  E:/mingw64/bin/../lib/gcc/x86_64-w64-mingw32/8.1.0/include
 //  E:/mingw64/bin/../lib/gcc/x86_64-w64-mingw32/8.1.0/include-fixed
-//  E:/mingw64/bin/../lib/gcc/x86_64-w64-mingw32/8.1.0/../../../../x86_64-w64-mingw32/include
+//  E:/mingw64/bin/../lib/gcc/x86_64-w64-mingw32/8.1.0/../../../../x86_64-w64-mingw32/include+
